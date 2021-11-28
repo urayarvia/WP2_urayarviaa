8ac34f7777a0db5d371c14544cd565409200bf20
urayarvia <uray.arvia1838@gmail.com>
2021-11-28 23:24:47 +0700

    pertemuan 12

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Aprina Situmorang 
+               saya tinggal di daerah Pontianak Timur
+               olah raga yang saya sukai adalah 
+               Bulutangkis";
+    }
+} 
